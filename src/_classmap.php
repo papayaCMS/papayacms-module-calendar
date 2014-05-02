0aafc4f375803b6c1e855960b1438b7c4bfa2b4e
Thomas Weinert <weinert@dimensional.de>
2014-05-02 15:17:04 +0200

    # Initial Commit, separate module from cms/framework repository

---
+++ src/_classmap.php
@@ -0,0 +1,14 @@
+<?php
+return array(
+  'actionbox_calendar_tag' => '/actbox_calendar_tag.php',
+  'actionbox_nextdates_tag' => '/actbox_nextdates_tag.php',
+  'base_calendar' => '/base_calendar.php',
+  'connector_calendar' => '/connector_calendar.php',
+  'content_calendar_editor' => '/content_calendar_editor.php',
+  'content_calendar_tag' => '/content_calendar_tag.php',
+  'datebox_html' => '/datebox_html.php',
+  'datebox_html_link' => '/datebox_html_link.php',
+  'edmodule_calendar' => '/edmodule_calendar.php',
+  'output_calendar' => '/output_calendar.php',
+  'papaya_calendar' => '/papaya_calendar.php',
+);
